3f6aff245206d1d67f2b663868810e90fcf0dcda
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-06-19 13:26:25 -0300

    criação da tela principal

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'movie_list_page.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
