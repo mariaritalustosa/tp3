8cf2479b992a4fdeb263dedd42c2d494fb1b75c9
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-06-19 12:59:45 -0300

    adiciona média de notas no movie model e cria tela de listagem de filmes populares

---
+++ lib/pages/movie_list_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:tp3/models/movie_model.dart';
+import 'package:tp3/services/api_service.dart';
+
+class MovieListPage extends StatefulWidget{
+  const MovieListPage({super.key});
+
+  @override
+  State<MovieListPage> createState() => _MovieListPageState();
+}
+
+class _MovieListPageState extends State <MovieListPage>{
+  late Future<List<Movie>> _futureMovies;
+  final ApiService _apiService = ApiService();
+
+  @override
+  void initState() {
+    super.initState();
+    _futureMovies = _apiService.fetchPopularMovies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Filmes Populares')),
+        body: FutureBuilder(
+          future: _futureMovies,
+           builder: (context,snapshot){
+             if(!snapshot.hasData || snapshot.data!.isEmpty){
+              return Center(
+                child: Text('Nenhum filme encontrado'),
+              );
+            } else{
+              final movies = snapshot.data!;
+              return ListView.builder(
+                itemCount: movies.length,
+                itemBuilder: (context, index) {
+                  final movie = movies[index];
+                   return ListTile(
+                  leading: movie.posterPath != null
+                      ? Image.network(
+                          'https://image.tmdb.org/t/p/w92${movie.posterPath!}',
+                          fit: BoxFit.cover,
+                        )
+                      : const Icon(Icons.movie),
+                  title: Text(movie.title),
+                  subtitle: Text('Nota: ${movie.voteAverage}'),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
