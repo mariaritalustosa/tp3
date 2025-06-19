bdad18b9eab7b6a204cfc3d61742f4644ad62630
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-06-19 14:06:12 -0300

    adiciona estrutura inicial para diálogo para comentário sobre o filme

--- lib/pages/movie_details_page.dart
+++ lib/pages/movie_details_page.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:tp3/models/movie_model.dart';
 
-class MovieDetailsPage {}
+class MovieDetailsPage extends StatelessWidget{
+  final Movie movie;
+
+  const MovieDetailsPage({super.key, required this.movie});
+  
+  void _showReviewDialog(BuildContext context){
+    double rating = 0;
+    final TextEditingController commentController = TextEditingController();
+
+    showDialog(
+      context: context, builder: builder)
+  }
+  }
+}
