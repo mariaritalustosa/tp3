6f0417a88979eb2355dbfbe662e227f99b21c7af
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-06-24 15:28:11 -0300

    criação da tabela Comments no banco de dados

--- lib/services/app_database.dart
+++ lib/services/app_database.dart
@@ -1,1 +1,14 @@
 import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+part 'app_database.g.dart';
+
+class Comments extends Table{
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get movieId => integer()();
+  RealColumn get rating => real()();
+  TextColumn get commentText => text()();
+}
